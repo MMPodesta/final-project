a37d488fc44e2bc37e1fb93373e925866939e084
MauricioMurta <mauriciomurta13@hotmail.com>
2024-01-28 18:01:19 +0000

    Better start screen

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def display_score():
     score_surface = test_font.render(f'Score: {current_time}', False, (64, 64, 64))
     score_rectangle = score_surface.get_rect(center=(400, 50))
     screen.blit(score_surface, score_rectangle)
+    return current_time
 
 
 pygame.init()
@@ -14,8 +15,9 @@ screen = pygame.display.set_mode((800, 400))  # width and height
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("assets/Pixeltype.ttf", 50)
-game_active = True
+game_active = False
 start_time = 0
+score = 0
 
 # Background
 sky_surface = pygame.image.load("assets/Sky.png").convert()
@@ -32,6 +34,17 @@ snail_rectangle = snail_surface.get_rect(midbottom=(600, 300))
 player_surface = pygame.image.load("assets/player_walk_1.png").convert_alpha()
 player_rectangle = player_surface.get_rect(midbottom=(80, 300))
 player_gravity = 0
+
+# Intro screen
+player_stand = pygame.image.load("assets/player_stand.png").convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+player_stand_rectangle = player_stand.get_rect(center=(400, 200))
+
+game_name = test_font.render("Pixel Runner", False, (111, 196, 169))
+game_name_rectangle = game_name.get_rect(center=(400, 80))
+
+game_message = test_font.render("Press space to run", False, (111, 196, 169))
+game_message_rectangle = game_message.get_rect(center=(400, 330))
 
 while True:
     for event in pygame.event.get():
@@ -57,10 +70,10 @@ while True:
         # Background
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
-        #pygame.draw.rect(screen, "#c0e8ec", score_rectangle)
-        #pygame.draw.rect(screen, "#c0e8ec", score_rectangle, 10)
-        #screen.blit(score_surface, score_rectangle)
-        display_score()
+        # pygame.draw.rect(screen, "#c0e8ec", score_rectangle)
+        # pygame.draw.rect(screen, "#c0e8ec", score_rectangle, 10)
+        # screen.blit(score_surface, score_rectangle)
+        score = display_score()
 
         # Snail
         snail_rectangle.x -= 4
@@ -78,7 +91,17 @@ while True:
         if snail_rectangle.colliderect(player_rectangle):
             game_active = False
     else:
-        screen.fill("Yellow")
+        screen.fill((94, 129, 162))
+        screen.blit(player_stand, player_stand_rectangle)
+
+        score_message = test_font.render(f'Your score: {score}', False, (111, 196, 169))
+        score_message_rectangle = score_message.get_rect(center=(400, 330))
+        screen.blit(game_name, game_name_rectangle)
+
+        if score == 0:
+            screen.blit(game_message, game_message_rectangle)
+        else:
+            screen.blit(score_message, score_message_rectangle)
 
     pygame.display.update()
     clock.tick(60)  # loop should not run faster than 60x/second
