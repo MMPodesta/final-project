fa61fcc043d1bc466e8d7bf39938e073c5dc3103
MauricioMurta <mauriciomurta13@hotmail.com>
2024-01-28 12:44:03 +0000

    Blank window + clock

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("test")
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800, 400))  # width and height
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    # draw all our elements
+    # update everything
+    pygame.display.update()
+    clock.tick(60)  # loop should not run faster than 60x/second
+
